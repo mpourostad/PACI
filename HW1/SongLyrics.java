6c8d7134d76706a769f49273bd6c9165f941179b
Mina Pourostad <minasmacbook@Minas-MacBook-Pro.local>
2021-02-27 18:00:34 -0500

    java file for hw 1

---
+++ HW1/SongLyrics.java
@@ -0,0 +1,32 @@
+public class SongLyrics
+/* Song Title: Human
+ * Singer: Rag'n'Bone Man
+ * https://www.youtube.com/watch?v=L3wKzyIN1yk&ab_channel=RagnBoneManVEVO 
+ */
+{
+	public static void main( String args[]) 
+	{
+		System.out.println( " I'm only human\n I'm only, I'm only\n I'm only human, human");
+		System.out.println();
+		System.out.println( " Maybe I'm foolish, Maybe I'm blind\n Thinking I can see through this\n And see what's behind");
+		System.out.println(" Got no way to prove it, so maybe I'm blind\n But I'm only human after all\n Don't put your blame on me\n Don't put your blame on me");
+		System.out.println();
+		System.out.println(" Take a look in the mirror\n And what do you see\n Do you see it clearer\n Or are you deceived\n In what you believe"); 
+		System.out.println(" 'Cause I'm only human after all\n You're only human after all\n Don't put the blame on me\n Don't put your blame on me");
+		System.out.println();
+		System.out.println(" Some people got the real problems\n Some people out of luck\n Some people think I can solve them\n Lord heavens above\n I'm only human after all");
+		System.out.println(" I'm only human after all\n Don't put the blame on me\n Don't put the blame on me");
+		System.out.println();
+		System.out.println(" Don't ask my opinion\n Don't ask me to lie\n Then beg for forgiveness\n For making you cry\n Making you cry\n 'Cause I'm only human after all");
+		System.out.println(" I'm only human after all\n Don't put your blame on me\n Don't put the blame on me");
+		System.out.println();
+		System.out.println(" Oh, some people got the real problems\n Some people out of luck\n Some people think I can solve them\n Lord heavens above");
+		System.out.println(" I'm only human after all\n I'm only human after all\n Don't put the blame on me\n Don't put the blame on me");
+		System.out.println(" I'm only human\n I make mistakes\n I'm only human\n That's all it takes\n To put the blame on me\n Don't put the blame on me");
+		System.out.println();
+		System.out.println(" I'm no prophet or Messiah\n Should go looking somewhere higher\n I'm only human after all\n I'm only human after all");
+		System.out.println(" Don't put the blame on me\n Don't put the blame on me");
+		System.out.println();
+		System.out.println(" I'm only human\n I do what I can\n I'm just a man\n I do what I can\n Don't put the blame on me\n Don't put your blame on me");
+	}
+}
